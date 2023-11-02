1232ae908bec64b502bf459968cf10e5159fad18
miekevandaelen <146383923+miekevandaelen@users.noreply.github.com>
2023-11-02 13:20:51 -0700

    Update project_v1.py

--- project_v1.py
+++ project_v1.py
@@ -5,23 +5,18 @@ from swagger_client.rest import ApiException
 from pprint import pprint
 
 # Configure OAuth2 access token for authorization: strava_oauth
-swagger_client.configuration.access_token = 'YOUR_ACCESS_TOKEN'
+swagger_client.configuration.access_token = '2b6366af9c3324ee576fd9d698fb25e191fd8e81'
 
 # create an instance of the API class
 api_instance = swagger_client.ActivitiesApi()
-name = name_example # String | The name of the activity.
-sportType = sportType_example # String | Sport type of activity. For example - Run, MountainBikeRide, Ride, etc.
-startDateLocal = 2013-10-20T19:20:30+01:00 # Date | ISO 8601 formatted date time.
-elapsedTime = 56 # Integer | In seconds.
-type = type_example # String | Type of activity. For example - Run, Ride etc. (optional)
-description = description_example # String | Description of the activity. (optional)
-distance = 3.4 # Float | In meters. (optional)
-trainer = 56 # Integer | Set to 1 to mark as a trainer activity. (optional)
-commute = 56 # Integer | Set to 1 to mark as commute. (optional)
+before = 56 # Integer | An epoch timestamp to use for filtering activities that have taken place before a certain time. (optional)
+after = 56 # Integer | An epoch timestamp to use for filtering activities that have taken place after a certain time. (optional)
+page = 56 # Integer | Page number. Defaults to 1. (optional)
+perPage = 56 # Integer | Number of items per page. Defaults to 30. (optional) (default to 30)
 
 try: 
-    # Create an Activity
-    api_response = api_instance.createActivity(name, sportType, startDateLocal, elapsedTime, type=type, description=description, distance=distance, trainer=trainer, commute=commute)
+    # List Athlete Activities
+    api_response = api_instance.getLoggedInAthleteActivities(before=before, after=after, page=page, perPage=perPage)
     pprint(api_response)
 except ApiException as e:
-    print("Exception when calling ActivitiesApi->createActivity: %s\n" % e)
+    print("Exception when calling ActivitiesApi->getLoggedInAthleteActivities: %s\n" % e)
