8d33c2d80eb1d1946b7ed78badf23ba309ffea71
miekevandaelen <146383923+miekevandaelen@users.noreply.github.com>
2023-11-12 11:30:24 -0800

    Update Strava_Spider.py

--- Strava_Spider.py
+++ Strava_Spider.py
@@ -1,50 +1,68 @@
-
-
 # to run 
 # scrapy crawl tmdb_spider -o movies.csv
 
+# Add these lines in settings
+# USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+# CLOSESPIDER_PAGECOUNT = 20
+# # Obey robots.txt rules
+# ROBOTSTXT_OBEY = False
+
 import scrapy
+# def authentication_failed(response):
+#     # TODO: Check the contents of the response and return True if it failed
+#     # or False if it succeeded.
+#     pass
 
-class TmdbSpider(scrapy.Spider):
-
-    name = 'tmdb_spider'
-    athlete = 16735685
-    strava_page = [f'https://www.strava.com/athletes/{athlete}']
-
+class ShareSpider(scrapy.Spider):
+    name = "sharespider"
+    start_urls = ['https://www.strava.com/login']
+    athlete = '16735685'
 
     def parse(self, response):
-
-
-        top_ten_page = response.url + '/segments/leader?top_tens=true'  
-        
-
+        yield scrapy.FormRequest.from_response(
+            response,
+            formdata={ 
+                'email': 'vandaelenmieke@gmail.com',
+                'password': 'Pic16Bproj!',
+            },
+            callback=self.after_login
+        )
+    def after_login(self, response):
+        top_ten_page = f'https://www.strava.com/athletes/{self.athlete}/segments/leader?top_tens=true'
         yield scrapy.Request(url=top_ten_page, callback=self.parse_top_tens)
 
-
+    # def parse(self, response):
+    #     # if authentication_failed(response):
+    #     #     self.logger.error("Login failed")
+    #     #     return
+    #     top_ten_page = response.url + f"/atheletes/{athlete}/segments/leader\?top_tens=tru"
+    #     # top_ten_page = 'https://www.strava.com/athletes/16735685/segments/leader?top_tens=true'
+    #     # top_ten_page = response.url + '/segments/leader\?top_tens=true'  
+    #     yield scrapy.Request(url=top_ten_page, callback=self.parse_top_tens)
 
 
     def parse_top_tens(self, response):
-
-
-        top_tens = response.css('div.tab-content: a::attr(href)').getall()
-
-
+        top_tens = response.css('table.my-segments tbody tr td a::attr(href)').getall()
+        # top_tens = response.css('div.tab-content: a::attr(href)').getall()
         for top_ten in top_tens:
-            yield scrapy.Request(url=top_ten, callback=self.parse_leaderboard)
+            if '/segments/' in top_ten:
+                # yield {"top ten": 'https://www.strava.com' + top_ten}
+                yield scrapy.Request(url='https://www.strava.com' + top_ten, callback=self.parse_leaderboard)
 
-
-
-    def parse_leaderbord(self, response):
+    def parse_leaderboard(self, response):
 
         athlete_pages = response.css('td.athlete.track-click a::attr(href)').getall()
-
         athlete = 16735685
-        target_account = f'/athletes/{athlete}'
+
+        target_account = f'/athletes/{self.athlete}'
 
         for athletes in athlete_pages:
             if(athletes != target_account):
-                yield {"Strava Page": athlete}
+                yield {"Strava Page": athletes}
             else:
                 break
+
+
+
 
 
