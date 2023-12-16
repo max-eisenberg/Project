66a9de9e07758c93fefcb901b8c61c7ed1ff2297
miekevandaelen <146383923+miekevandaelen@users.noreply.github.com>
2023-12-15 17:06:07 -0800

    -

--- Strava_Spider.py
+++ Strava_Spider.py
@@ -17,14 +17,14 @@ class StravaScraper(scrapy.Spider):
     allowed_domains = ['strava.com']
     start_urls = ['https://www.strava.com/login']
     
-
+    # To define month for athlete url 
     def __init__(self):
         self.date_dict = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08',
                           'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
         self.month_before = {'Jan':'12','Feb':'01','Mar':'02','Apr':'03','May':'04','Jun':'05','Jul':'06','Aug':'07',
                           'Sep':'08','Oct':'09','Nov':'10','Dec':'11'}
 
-
+    #Login to strava
     def parse(self, response):
         token = response.xpath('//*[@name="csrf-token"]/@content').get()
         return FormRequest.from_response(response,
@@ -38,13 +38,12 @@ class StravaScraper(scrapy.Spider):
                                         #eta={'dont_redirect': True, 'handle_httpstatus_list': [302]},
                                         callback=self.parse_after_login)
     
-
+    #Direct to athlete top ten page
     def parse_after_login(self, response):
-        #Try except statement for if user has no top tens
         top_ten_page = f'https://www.strava.com/athletes/{self.athlete}/segments/leader?top_tens=true'
-    
         yield scrapy.Request(url=top_ten_page, callback=self.parse_top_tens)
 
+    # Yield all top ten segments on page, direct to next page and repeat
     def parse_top_tens(self, response):
         top_tens = response.css('table.my-segments tbody tr td a::attr(href)').getall()
         self.segments.extend(top_tens)
@@ -55,25 +54,19 @@ class StravaScraper(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, callback = self.parse_top_tens)
             
         for top_ten in self.segments:
-
+            #Make sure link is a segment link 
             if '/segments/' in top_ten:
-            # yield {"top ten": 'https://www.strava.com' + top_ten}
                 top_ten = 'https://www.strava.com' + top_ten
                 yield scrapy.Request(url=top_ten, callback = self.parse_leaderboard)
-    
+    # get athlete links from segments page
     def parse_leaderboard(self, response):
         self.counter +=1
         self.url_list = [] 
         athlete_pages = response.css('td.athlete.track-click a::attr(href)').getall()
         dates = response.css('a[href^="/segment_efforts/"]::text').getall()
-        #This get month of acheivement, we want month before acheivement
+        # month before achievement
         edited_dates = []
-        # for d in dates:
-        #     month = self.date_dict[d[:3]]
-        #     year = d[-4:]
-        #     edited_dates.append(year+month)
-
-        #New code: Defined new dictionary that gives month before, if month before is december, also subtracts one from year.
+        # Record date of achievement in numerical form
         for d in dates:
             month = self.month_before[d[:3]]
             if month == "12":
@@ -85,14 +78,15 @@ class StravaScraper(scrapy.Spider):
 
         athlete = '16735685'  # Ensure this is a string
 
-        athletes_data = {}  # Dictionary to store athlete URLs and corresponding dates
-
+        athletes_data = {}  
+        #Stop recording athlete links at user (Only get links for people who beat user)
+        #Store athlete url and date of achievement together
         for i, athlete_url in enumerate(athlete_pages):
             if athlete_url.endswith(f'/athletes/{athlete}'):
                 break
             athlete_url = 'https://www.strava.com' + athlete_url
             athletes_data[athlete_url] = edited_dates[i]
-
+        #Format athlete interval link
         for athlete_url, date in athletes_data.items():
             interval = str(date)
         
--- strava_selenium(2).py
+++ strava_selenium(2).py
@@ -7,14 +7,16 @@ import time
 import pandas as pd
 
 activities = pd.DataFrame()
+#Testing links
 test = ['https://www.strava.com/athletes/29127886#interval_type?chart_type=miles&interval_type=month&interval=202012&year_offset=3','https://www.strava.com/athletes/5838343#interval_type?chart_type=miles&interval_type=month&interval=201803&year_offset=5','https://www.strava.com/athletes/28626466#interval_type?chart_type=miles&interval_type=month&interval=202205&year_offset=1']
 
+#Actual data,, Set up chrome driver for selenium
 urls = pd.read_csv('Interval_pages.csv')
 urls_list = urls['Athlete page']
 executable_path="/Users/sasha/Documents/PIC16B/strava_scraper/strava_scraper/spiders/chromedriver"
 driver = webdriver.Chrome(executable_path=executable_path)
         
-
+#Login to strava
 driver.get('https://www.strava.com/login')
 email_field = driver.find_element_by_id('email')
 email_field.send_keys('sashaprs@gmail.com')
@@ -23,12 +25,15 @@ password_field.send_keys('PIC16BProj')
 login_button = driver.find_element_by_id('login-button')
 login_button.click()
 
+#Collect activity data
 for url in urls_list:
     temp=[]
     activity_stats = [] 
     names = []
     driver.get(url)
     driver.implicitly_wait(10)
+    #Only get data drom public profiles
+    #Store name, distance, elevation, time
     try:
         athlete = driver.find_element(By.CSS_SELECTOR, "h1.text-title1.athlete-name").text
         name = driver.find_elements(By.XPATH("//a[@data-testid='owners-name'"))
